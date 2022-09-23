cadac7918f2b011be27b368f9234fbe36ec00609
rinonkia <git@gatapon.me>
2022-09-24 01:34:13 +0900

    DBからレコードを取得するテスト

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/rinonkia/go_api_tutorial/handlers"
+	"github.com/rinonkia/go_api_tutorial/models"
 	"log"
 	"net/http"
 
@@ -26,9 +27,39 @@ func main() {
 
 	if err := db.Ping(); err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println("connect to DB")
 	}
+	fmt.Println("connect to DB")
+
+	const sqlStr = `
+		select *
+		from articles;
+	`
+
+	rows, err := db.Query(sqlStr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer rows.Close()
+
+	articleArray := make([]models.Article, 0)
+	for rows.Next() {
+		var article models.Article
+		var createdTime sql.NullTime
+		err := rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+
+		if createdTime.Valid {
+			article.CreatedAt = createdTime.Time
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			articleArray = append(articleArray, article)
+		}
+	}
+
+	fmt.Printf("%+v\n", articleArray)
 
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
